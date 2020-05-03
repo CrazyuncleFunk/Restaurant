f7f49c44a9e4f02ff725586350caec03ebdf0476
erin dunn <erindunn@LIVE.CO.UK>
2020-05-03 22:07:53 +0100

    new

--- Spice/Spice/Utility/SD.cs
+++ Spice/Spice/Utility/SD.cs
@@ -9,12 +9,26 @@ namespace Spice.Utility
     public static class SD
     {
         public const string DefaultFoodImage = "default_food.png";
+
         public const string ManagerUser = "Manager";
         public const string KitchenUser = "Kitchen";
         public const string FrontDeskUser = "FrontDesk";
         public const string CustomerEndUser = "Customer";
+
         public const string ssShoppingCartCount = "ssCartCount";
         public const string ssCouponCode = "ssCouponCode";
+
+        public const string StatusSubmitted= "Submitted";
+        public const string StatusInProcess = "Being Prepared";
+        public const string StatusReady = "Ready for Pickup";
+        public const string StatusCompleted = "Completed";
+        public const string StatusCancelled = "Cancelled";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+
+
 
 
 
